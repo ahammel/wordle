933b2037f1f054db84e9bddc3a012dedc2a4b5cf
Alex Hammel <alex@opentech.eco>
2022-04-06 21:27:26 -0700

    Add guess-word and guess-result->wordprint

--- src/ahammel/wordle.clj
+++ src/ahammel/wordle.clj
@@ -1,12 +1,60 @@
 (ns ahammel.wordle
+  (:require [clojure.set :as set])
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(defn guess-word
+  [answer guess]
+  (let [correct   (->> (map (fn [a g] (when (= a g) g))
+                            answer
+                            guess)
+                       (map-indexed vector)
+                       (filter (fn [[_ letter]] (some? letter)))
+                       (into {}))
+        unguessed (->> (map-indexed vector answer)
+                       (filter (fn [[ix _]] (not (correct ix))))
+                       (map second)
+                       set)
+        wrong-pos (->> (map (fn [a g]
+                              (when (and (not= a g) (unguessed g))
+                                g))
+                            answer
+                            guess)
+                       (map-indexed vector)
+                       (filter (fn [[_ letter]] (some? letter)))
+                       (into {}))
+        not-in    (set/difference (set guess)
+                                  (set answer))]
+    {:wordle/correct   correct
+     :wordle/wrong-pos wrong-pos
+     :wordle/not-in    not-in
+     :wordle/size      (count answer)}))
+
+(def ^:private conj-some
+  ((remove nil?) conj))
+
+(defn guess-result->wordprint
+  [{:wordle/keys [correct not-in wrong-pos size]}]
+  (let [wrong (->> (range size)
+                   (map (fn [i]
+                          [i {:wordle/is-not (conj-some
+                                               not-in
+                                               (get wrong-pos i))}]))
+                   (into {}))
+        right (->> correct
+                   (map
+                     (fn [[k v]]
+                       [k {:wordle/is v}]))
+                   (into {}))]
+    {:wordle/positions (merge wrong right)
+     :wordle/unguessed (->> wrong-pos
+                            vals
+                            set)}))
+
+(comment
+  (guess-result->wordprint
+    (guess-word "loops" "blood")))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (greet {:name (first args)}))
+  (println "hi"))
--- test/ahammel/wordle_test.clj
+++ test/ahammel/wordle_test.clj
@@ -1,7 +1,67 @@
 (ns ahammel.wordle-test
-  (:require [clojure.test :refer :all]
-            [ahammel.wordle :refer :all]))
+  (:require [clojure.test :refer [deftest testing is are]]
+            [ahammel.wordle :refer [guess-word
+                                    guess-result->wordprint]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest guess-word-test
+  (testing "guess-word"
+    (are [args result]
+         (= result
+            (guess-word (:answer args) (:guess args)))
+
+         {:answer "guess" :guess "guess"}
+         {:wordle/correct   {0 \g
+                             1 \u
+                             2 \e
+                             3 \s
+                             4 \s}
+          :wordle/wrong-pos {}
+          :wordle/not-in    #{}
+          :wordle/size      5}
+
+         {:answer "ropes" :guess "robes"}
+         {:wordle/correct   {0 \r
+                             1 \o
+                             3 \e
+                             4 \s}
+          :wordle/wrong-pos {}
+          :wordle/not-in    #{\b}
+          :wordle/size      5}
+
+         {:answer "loops" :guess "blood"}
+         {:wordle/correct   {2 \o}
+          :wordle/wrong-pos {1 \l
+                             3 \o}
+          :wordle/not-in    #{\b \d}
+          :wordle/size      5})))
+
+(deftest guess-result->wordprint-test
+  (testing "guess-result->wordprint"
+    (are [args result]
+         (= result
+            (guess-result->wordprint
+              (guess-word (:answer args) (:guess args))))
+
+         {:answer "guess" :guess "guess"}
+         {:wordle/positions {0 {:wordle/is \g}
+                             1 {:wordle/is \u}
+                             2 {:wordle/is \e}
+                             3 {:wordle/is \s}
+                             4 {:wordle/is \s} }
+          :wordle/unguessed  #{}}
+
+         {:answer "ropes" :guess "robes"}
+         {:wordle/positions {0 {:wordle/is \r}
+                             1 {:wordle/is \o}
+                             2 {:wordle/is-not #{\b}}
+                             3 {:wordle/is \e}
+                             4 {:wordle/is \s} }
+          :wordle/unguessed  #{}}
+
+         {:answer "loops" :guess "blood"}
+         {:wordle/positions {0 {:wordle/is-not #{\b \d}}
+                             1 {:wordle/is-not #{\b \d \l} }
+                             2 {:wordle/is \o}
+                             3 {:wordle/is-not #{\b \d \o}}
+                             4 {:wordle/is-not #{\b \d}} }
+          :wordle/unguessed  #{\l \o}})))
